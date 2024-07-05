4ddc70ea4ac8b5918ef29a457221949c4ecf1fc2
CLMNTDFR <deferclement59@gmail.com>
2024-07-05 18:26:50 +0200

    editing front end to make it more responsive and adding a "netflix design"

--- django-web-app/merchex/authentification/models.py
+++ django-web-app/merchex/authentification/models.py
@@ -10,5 +10,4 @@ class User(AbstractUser):
         (CREATOR, 'Creator'),
         (SUBSCRIBER, 'Subscriber'),
     )
-    profile_photo = models.ImageField(verbose_name='profile picture')
     role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Role')
